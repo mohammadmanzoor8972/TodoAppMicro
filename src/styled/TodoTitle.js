57a8d175192b1410da3321a0f2259dd7072536b9
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-06-23 11:09:26 +0530

    Added styled Component for Title

---
+++ src/styled/TodoTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+
+const TodoTitle = styled.div`
+font-size:20px;
+color:deepblue;
+padding:10px;
+font-weight:bold;
+`
+export default TodoTitle;
