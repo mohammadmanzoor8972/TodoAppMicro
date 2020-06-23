c593e9e4faa920c3d84f066cf487e510cf3fcd60
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-06-23 11:21:38 +0530

    Updates

---
+++ precache-manifest.5a1529405338f608b6ac063693d0bfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698fc3b86d5abcc7b95f5a458c6e791b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606711161442ffb1e650",
+    "url": "/static/css/main.bf556ce3.chunk.css"
+  },
+  {
+    "revision": "f2b0b91b6c050a760cf7",
+    "url": "/static/js/2.f568c0c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f568c0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606711161442ffb1e650",
+    "url": "/static/js/main.8d13933d.chunk.js"
+  },
+  {
+    "revision": "04adc46759be77719189",
+    "url": "/static/js/runtime-main.7d7fde0b.js"
+  }
+]);
