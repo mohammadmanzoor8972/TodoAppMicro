b126f37ae21a1bcbf5c195ebe455fb5c34b25afe
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-06-23 11:32:38 +0530

    Updates

---
+++ precache-manifest.643c42ac6b79362aa7a6b37ee1b52eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621c952b98b256d1bb7c5437a0906db8",
+    "url": "/TodoAppMicro/index.html"
+  },
+  {
+    "revision": "f37c9dd1d52150abb07f",
+    "url": "/TodoAppMicro/static/css/main.bf556ce3.chunk.css"
+  },
+  {
+    "revision": "9cfdf5d66da9a77a59f8",
+    "url": "/TodoAppMicro/static/js/2.2b602524.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoAppMicro/static/js/2.2b602524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37c9dd1d52150abb07f",
+    "url": "/TodoAppMicro/static/js/main.03e2f182.chunk.js"
+  },
+  {
+    "revision": "e872a536c8d139eed3f0",
+    "url": "/TodoAppMicro/static/js/runtime-main.b84b2518.js"
+  }
+]);
